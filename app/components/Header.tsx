57ebc90a22cef194a2817518e27c3599b661220b
kylesullyman <kylsullivan@chapman.edu>
2025-02-05 16:12:14 -0800

    First commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,22 @@
+// components/Header.tsx
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-background text-foreground p-4">
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
